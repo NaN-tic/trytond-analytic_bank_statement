6acbf9ff9a6dc8c03f4adf5c44e671b2ace34817
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-08-16 10:27:08 +0200

    Upgrade test

--- tests/test_analytic_bank_statement.py
+++ tests/test_analytic_bank_statement.py
@@ -3,23 +3,13 @@
 import doctest
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('analytic_bank_statement')
-
-    def test0005views(self):
-        'Test views'
-        test_view('analytic_bank_statement')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'analytic_bank_statement'
 
 
 def suite():
